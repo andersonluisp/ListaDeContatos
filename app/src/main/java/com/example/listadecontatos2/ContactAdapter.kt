7d0919c3bf28db922da9753f5b517a62ab72320c
andersonluisp <andersonluiscp@gmail.com>
2021-04-18 11:40:28 -0300

    Criação do layout do card e implementação da class onCreateViewHolder

--- app/src/main/java/com/example/listadecontatos2/ContactAdapter.kt
+++ app/src/main/java/com/example/listadecontatos2/ContactAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.listadecontatos2
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -9,12 +10,14 @@ class ContactAdapter : RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHol
     //<Contact> é a classe modelo dos itens da lista
     private val list: MutableList<Contact> = mutableListOf()
 
+    // Responsável por criar cada item visual da nossa tela
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactAdapterViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.contact_item, parent, false)
+        return ContactAdapterViewHolder(view)
     }
 
-    //Rodar em cada item do Array, obter o valor em cada item e preencher na tela.
-    // Vai popular o item no recycler view
+    // Rodar em cada item do Array, obter o valor em cada item e preencher na tela.
+    // Vai popular o item no recycler view com os itens do onCreateViewHolder
     override fun onBindViewHolder(holder: ContactAdapterViewHolder, position: Int) {
         //Precisa passar por todos os itens gerenciaods pela class ContactAdapterViewHolder
         holder.bind(list[position])
